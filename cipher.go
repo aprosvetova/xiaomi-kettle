f0a9350c38d9c5f05fc0c10912a34ca958c2cc03
ninedraft <clamores.pro@gmail.com>
2018-11-29 21:18:37 +0300

    remove unnecessary conversions

--- cipher.go
+++ cipher.go
@@ -2,11 +2,11 @@ package kettle
 
 func CipherInit(key []byte) []byte {
 	var perm = make([]uint8, 0, 256)
-	perm = append(perm, []uint8(key)...)
+	perm = append(perm, key...)
 	var j uint8
 	keyLen := uint8(len(key))
 	for ia := range perm {
-		j += uint8(perm[ia] + key[uint8(ia)%keyLen])
+		j += perm[ia] + key[uint8(ia)%keyLen]
 		perm[ia], perm[j] = perm[j], perm[ia]
 	}
 	return perm
@@ -17,7 +17,7 @@ func CipherCrypt(input, perm []byte) []byte {
 	output := make([]byte, len(input))
 	for i := 0; i < len(input); i++ {
 		index1++
-		index2 += uint8(perm[index1])
+		index2 += perm[index1]
 		perm[index1], perm[index2] = perm[index2], perm[index1]
 		idx := perm[index1] + perm[index2]
 		output[i] = input[i] ^ perm[idx]
